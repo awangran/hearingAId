c93f311709d4540e7bb4cf58d13fbee4a559dd98
awangran <ashleero.yin@gmail.com>
2024-11-10 14:30:14 -0500

    routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,22 @@
+import React from "react";
+import {Routes, Route} from 'react-router-dom'
+import './index.css'
+import Dashboard from "./routes/Dashboard";
+import Generate from "./routes/Generate";
+import Login from "./routes/Login";
+import Root from "./routes/Root";
+import Signin from "./routes/Signin";
 
 export default function App() {
   return (
-    <></>
+    <>
+    <Routes>
+      <Route path='/' element={<Root />} />
+      <Route path='/signin' element={<Signin />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/dashboard' element={<Dashboard />} />
+      <Route path='/generate' element={<Generate />} />
+    </Routes>
+    </>
   );
 }
