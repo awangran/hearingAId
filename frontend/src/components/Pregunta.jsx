dab6aeaa17f889bac774319dc824319dbf458db2
awangran <ashleero.yin@gmail.com>
2024-11-13 12:34:31 -0500

    caja de preguntas

---
+++ frontend/src/components/Pregunta.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Pregunta() {
+  return (
+    <div className='mx-16 bg-gray-700 rounded-lg h-72  text-center'>
+        <textarea className='h-72 w-full bg-gray-700 text-7xl	 rounded-lg p-12'/>
+
+    </div>
+  )
+}
+
+export default Pregunta
--- frontend/src/routes/Generate.jsx
+++ frontend/src/routes/Generate.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Navbar2 from '../components/Navbar2'
 import { Button } from '@material-tailwind/react'
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import Pregunta from '../components/Pregunta';
 
 
 function Generate() {
@@ -16,6 +17,11 @@ function Generate() {
   if (!browserSupportsSpeechRecognition) {
     return <span>Tu navegador no soporta la funcion de voz a texto.</span>;
   }
+
+  const subs = transcript.split(' ').slice(-15).join(' ');
+
+  const [openPregunta, setOpenPregunta] = React.useState(false);
+
 
 
 
@@ -33,7 +39,7 @@ function Generate() {
 
       <div className='mx-16 bg-gray-200 p-12 rounded-lg h-64 text-center	'>
         <p className="mt-2 text-pretty text-2xl font-semibold tracking-tight text-gray-700 sm:text-4xl lg:text-balance">
-          {transcript}
+          {subs}
           </p>
       </div>
 
@@ -42,8 +48,13 @@ function Generate() {
         <Button variant="filled" onClick={()=>{SpeechRecognition.stopListening({continuous: false}, console.log(transcript));
  }}>Terminar</Button>
         <Button variant="filled">Generar</Button>
-        <Button variant="outlined">Preguntar</Button>
+        <Button variant="outlined" onClick={()=> setOpenPregunta(!openPregunta)}>Preguntar</Button>
     </div>
+    { openPregunta ? (
+      <Pregunta />
+    ):(
+      <></>
+    )}
 
     </>
   )
